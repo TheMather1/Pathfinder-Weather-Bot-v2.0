b125ba111b70c348e6108c8f1844ae2b06f8a250
Mathias Sand Jahren <the.mather1@gmail.com>
2019-09-08 17:45:07 +0200

    Debug bot logic.

--- src/main/kotlin/pathfinder/weatherBot/Bot.kt
+++ src/main/kotlin/pathfinder/weatherBot/Bot.kt
@@ -1,5 +1,6 @@
 package pathfinder.weatherBot
 
+import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.Activity.watching
@@ -26,29 +27,32 @@ class Bot(private val guildId: String){
     private val outputChannel: TextChannel?
         get() = guild?.getTextChannelById(outputChannelId ?: "")
     val location = Location()
-    private val clock = Clock(this)
+    private val clock: Clock by lazy { Clock(this) }
 
     companion object: ListenerAdapter() {
         private const val DEFAULT_PREFIX = "w!"
         private lateinit var token: String
         private val instances = HashMap<String, Bot>()
         private val logger = LoggerFactory.getLogger(Bot::class.java)
-        private val botUser = JDABuilder(token)
-                .setActivity(watching("the weather"))
-                .build()
+        lateinit var botUser: JDA
 
         @JvmStatic
         fun main(args: Array<String>) {
             token = args[0]
+            botUser = JDABuilder(token)
+                    .setActivity(watching("the weather"))
+                    .build()
             botUser.addEventListener(this)
         }
 
         override fun onMessageReceived(event: MessageReceivedEvent) {
-            event.message.apply {
+            event.message.run {
                 logger.info("Received message:\n$author: $contentRaw in $guild#$channel")
-                instances[guild.id]?.takeIf {
+                instances.getOrPut(guild.id, {Bot(guild.id)}).takeIf {
                     contentRaw.startsWith(it.prefix)
-                }?.command(this)
+                }?.let {
+                    it.post(this.textChannel, it.command(this))
+                }
             }
         }
 
@@ -64,13 +68,13 @@ class Bot(private val guildId: String){
             instances.remove(event.guild.id)?.stop()
         }
 
-        private fun helpText(message: String) = when(message){
+        private fun helpText(message: String): String = when(message){
             "" -> listCommands()
             "channel" -> "Usage: channel [#channel] - Redirects weather output to the specified channel."
             else -> "Unrecognized command - $message."
         }
 
-        private fun listCommands() = "help - You're looking at it. List available commands.\n" +
+        private fun listCommands(): String = "help - You're looking at it. List available commands.\n" +
                 "help [command] - Display more information on the command."
     }
 
@@ -78,7 +82,7 @@ class Bot(private val guildId: String){
         message.apply {
             val command = contentRaw.substringAfter(prefix).substringBefore(" ").toLowerCase()
             val sudo = guild.getMember(author)!!.hasPermission(Permission.ADMINISTRATOR)
-            val param = contentRaw.substringAfter(command)
+            val param = contentRaw.substringAfter(command).substringAfter(' ')
             return when (command){
                 "help" -> helpText(param)
                 "start" -> if(sudo) start() else "You do not have permission to start the bot."
@@ -104,26 +108,34 @@ class Bot(private val guildId: String){
                 ?: "Cannot recognize channel. Correct usage: channel [#channel]"
     }
 
-    private fun start(): String = if (location.isSet()) clock.start() else "You first need to set the ${location.missing()}."
-    private fun stop(): String = clock.stop()
+    private fun start(): String = if (location.isSet()) clock.start()
+            else "You first need to set the ${location.missing()}."
+    private fun stop(): String = if ((::clock.getDelegate() as Lazy<*>).isInitialized()) clock.stop() else ""
 
     private fun setClimate(message: String): String =
-        Climate.valueOf(message.toUpperCase()).let {
-            location.climate = it
-            "Climate set to $it."
-        }
+        try {
+            Climate.valueOf(message.toUpperCase()).let {
+                location.climate = it
+                "Climate set to $it."
+            }
+        } catch (e: IllegalArgumentException) { "Invalid climate type." }
 
     private fun setElevation(message: String): String =
-        Elevation.valueOf(message.toUpperCase()).let {
-            location.elevation = it
-            "Elevation set to $it."
-        }
+        try {
+            Elevation.valueOf(message.toUpperCase()).let {
+                location.elevation = it
+                "Elevation set to $it."
+            }
+        } catch (e: IllegalArgumentException) { "Invalid elevation type." }
 
     private fun setDesert(message: String): String =
-            message.toBoolean().let {
-                location.desert = it
-                "Desert set to $it."
-            }
+            try {
+                message.toBoolean().let {
+                    location.desert = it
+                    "Desert set to $it."
+                }
+            } catch (e: java.lang.IllegalArgumentException) { "Invalid parameter. Desert must be either TRUE or FALSE. "}
 
-    fun post(message: String) = outputChannel?.sendMessage(message)
+    fun post(message: String) = outputChannel?.sendMessage(message)?.complete()
+    fun post(channel: TextChannel, message: String) = channel.sendMessage(message).complete()!!
 }
--- src/main/kotlin/pathfinder/weatherBot/location/Location.kt
+++ src/main/kotlin/pathfinder/weatherBot/location/Location.kt
@@ -1,5 +1,6 @@
 package pathfinder.weatherBot.location
 
+import pathfinder.weatherBot.weather.precipitation.Intensity
 import java.lang.RuntimeException
 
 
@@ -7,13 +8,13 @@ class Location {
     lateinit var climate : Climate
     lateinit var elevation : Elevation
     var desert = false
-    val adjustPrecip = climate.adjustPrecip + elevation.adjustPrecip
-    val intensity = elevation.basePrecipitation + climate.adjustPrecip
-    val frequency = climate.adjustPrecip
+    val adjustPrecip: Int by lazy{climate.adjustPrecip + elevation.adjustPrecip}
+    val intensity: Intensity by lazy{elevation.basePrecipitation + climate.adjustPrecip}
+    val frequency: Int by lazy {climate.adjustPrecip}
     var snowLevel: Double = 0.0
 
     fun isSet(): Boolean = ::climate.isInitialized && ::elevation.isInitialized
-    fun missing(): String = when (::climate.isInitialized to ::elevation.isInitialized){
+    fun missing(): String = when (!::climate.isInitialized to !::elevation.isInitialized){
             true to true -> "climate and elevation"
             true to false -> "climate"
             false to true -> "elevation"
